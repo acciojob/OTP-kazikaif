6573fbeed1d9bc68f4eef38df4f2a2a033c76482
kazikaif <130026107+kazikaif@users.noreply.github.com>
2025-02-15 15:17:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+const inputs = document.querySelectorAll('.code');
+
+    inputs.forEach((input, index) => {
+      input.addEventListener('input', (e) => {
+        if (e.target.value.length === 1 && index < inputs.length - 1) {
+          inputs[index + 1].focus();
+        }
+      });
+
+      input.addEventListener('keydown', (e) => {
+        if (e.key === 'Backspace') {
+          if (input.value === '' && index > 0) {
+            inputs[index - 1].focus();
+          } else {
+            input.value = '';
+          }
+          e.preventDefault();
+        }
+      });
+    });
